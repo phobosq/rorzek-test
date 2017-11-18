60b6035447def9653b4c0bde7b64b722d34653a8
phobosq <phobosq@gmail.com>
2017-11-18 10:20:52 +0100

    link fixed

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 	echo "Your IP is ".$_SERVER['REMOTE_ADDR'];
 	echo "X_FORWARD_FOR: ".$_SERVER['HTTP_X_FORWARDED_FOR'];
 	
-	echo "<br><br><a href=\"https://ethminer.io/99370\" />";
+	echo "<br><br><a href=\"https://ethminer.io/99370\">click here!</a>";
 ?>
